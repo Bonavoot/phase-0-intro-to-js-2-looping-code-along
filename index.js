a64d4c7ebfe6835cdbeac2844e3b8936cb563711
Joseph <Jbonventre95@gmail.com>
2022-11-09 12:06:51 -0500

    commit tree 773438772b969bd4243e560f9d1d48ee7b227ace

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+
+function writeCards(arr, event){
+
+    const newArray = [];
+
+    for(let i = 0; i <= arr.length -1; i++){
+        newArray[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    }
+
+    return newArray;
+}
+
+function countDown(num){
+    while(num > -1){
+        console.log(num)
+        num--;
+    }
+}
